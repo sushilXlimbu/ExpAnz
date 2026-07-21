57ddd9f4717dd4c12c982248ec39ffc3d7e3286b
sushilXlimbu <sushilsuhang0035@gmail.com>
2026-07-21 16:29:39 +0100

    documentation

--- BudgetAnalyzer/BudgetAnalyzer.Api/Program.cs
+++ BudgetAnalyzer/BudgetAnalyzer.Api/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
-        policy.WithOrigins("http://localhost:5174")
+        policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
